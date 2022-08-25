41826ca52e3480e7ae0b6df94006f9597c57454a
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-08-25 09:58:23 -0400

    botones, form, ts, prop, otros

--- src/app/segunda/segunda.page.ts
+++ src/app/segunda/segunda.page.ts
@@ -5,11 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './segunda.page.html',
   styleUrls: ['./segunda.page.scss'],
 })
+// TYPESCRIPT
 export class SegundaPage implements OnInit {
+  apruebo : number;
+  rechazo : number;
+  otro : number;
 
-  constructor() { }
+  constructor() {
+    this.apruebo = 0;
+    this.rechazo = 0;
+    this.otro    = 0; 
+  }
 
   ngOnInit() {
   }
 
+  botonApruebo()
+  {
+    this.apruebo = this.apruebo + 1;
+  }
+  botonRechazo()
+  {
+    this.rechazo = this.rechazo + 1;
+  }
+  botonOtro()
+  {
+    this.otro = this.otro + 1;
+  }
 }
